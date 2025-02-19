cb576df580f2907c8c9fc5f56864534a713e97df
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2025-02-19 11:44:01 -0700

    [Primer] comit para iniciar este repo

---
+++ mi_primera_app_storyboard/mi_primera_app_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mi_primera_app_storyboard
+//
+//  Created by alumno on 2/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
